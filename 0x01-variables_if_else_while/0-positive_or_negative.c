7bbb75ce51c391f7e2aae1c6112c17cd9f183ca7
kuriakariuki <kariukijohnk1@gmail.com>
2022-03-11 05:26:57 -0800

    Check if a number is zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ *  Return: Always o (Success)
+ */
+
+int main(void)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+
+}
